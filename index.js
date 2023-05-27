31ba220f536d2020145208844a3bb6fdc78116e2
Udin Saputra <132532759+udinsp@users.noreply.github.com>
2023-05-27 07:46:04 +0700

    add header check

--- index.js
+++ index.js
@@ -68,22 +68,7 @@ app.post('/interactions', verifyKeyMiddleware(PUBLIC_KEY), async (req, res) => {
         const response = await axios.get(`https://ipapi.co/${ipAddress}/json/`);
         const ipData = response.data;
 
-        const formattedData = `
-        IP Address: ${ipData.ip}
-        Network: ${ipData.network}
-        Version: ${ipData.version}
-        City: ${ipData.city}
-        Region: ${ipData.region}
-        Country: ${ipData.country_name}
-        Continent: ${ipData.continent_code}
-        Latitude: ${ipData.latitude}
-        Longitude: ${ipData.longitude}
-        Timezone: ${ipData.timezone}
-        Currency: ${ipData.currency}
-        Languages: ${ipData.languages}
-        ASN: ${ipData.asn}
-        Organization: ${ipData.org}
-        `;
+        const formattedData = `IP Address: ${ipData.ip}\nNetwork: ${ipData.network}\nVersion: ${ipData.version}\nCity: ${ipData.city}\nRegion: ${ipData.region}\nCountry: ${ipData.country_name}\nContinent: ${ipData.continent_code}\nLatitude: ${ipData.latitude}\nLongitude: ${ipData.longitude}\nTimezone: ${ipData.timezone}\nCurrency: ${ipData.currency}\nLanguages: ${ipData.languages}\nASN: ${ipData.asn}\nOrganization: ${ipData.org}`;
 
         return res.send({
           type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
@@ -113,25 +98,7 @@ app.post('/interactions', verifyKeyMiddleware(PUBLIC_KEY), async (req, res) => {
         const response = await axios.get(`https://www.ipqualityscore.com/api/json/phone/${API_IPQUALITYSCORE}/${number}`);
         const numberData = response.data;
 
-        const formattedData = `
-        Formatted: ${numberData.formatted}
-        Local Format: ${numberData.local_format}
-        Valid: ${numberData.valid}
-		Name: ${numberData.name}
-		Leaked: ${numberData.leaked}
-		Spammer: ${numberData.spammer}
-        Fraud Score: ${numberData.fraud_score}
-        Recent Abuse: ${numberData.recent_abuse}
-        VOIP: ${numberData.VOIP}
-        Prepaid: ${numberData.prepaid}
-        Risky: ${numberData.risky}
-        Active: ${numberData.active}
-        Carrier: ${numberData.carrier}
-        Line Type: ${numberData.line_type}
-        Country: ${numberData.country}
-        Dialing Code: ${numberData.dialing_code}
-        Timezone: ${numberData.timezone}
-        `;
+        const formattedData = `Formatted: ${numberData.formatted}\nLocal Format: ${numberData.local_format}\nValid: ${numberData.valid}\nName: ${numberData.name}\nLeaked: ${numberData.leaked}\nSpammer: ${numberData.spammer}\nFraud Score: ${numberData.fraud_score}\nRecent Abuse: ${numberData.recent_abuse}\nVOIP: ${numberData.VOIP}\nPrepaid: ${numberData.prepaid}\nRisky: ${numberData.risky}\nActive: ${numberData.active}\nCarrier: ${numberData.carrier}\nLine Type: ${numberData.line_type}\nCountry: ${numberData.country}\nDialing Code: ${numberData.dialing_code}\nTimezone: ${numberData.timezone}`;
 
         return res.send({
           type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
@@ -351,6 +318,62 @@ app.post('/interactions', verifyKeyMiddleware(PUBLIC_KEY), async (req, res) => {
         });
       }
     }
+	
+	if (interaction.data.name === 'pagelinks') {
+      const url = interaction.data.options[0].value;
+      try {
+        const response = await axios.get(`https://api.hackertarget.com/pagelinks/?q=${encodeURIComponent(url)}`);
+        const resultpagelinks = response.data;
+
+        return res.send({
+          type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+          data: {
+            embeds: [
+              {
+                description: resultpagelinks,
+                color: null
+              }
+            ]
+          }
+        });
+      } catch (error) {
+        console.log(error);
+        return res.send({
+          type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+          data: {
+            content: 'Failed to Extract Links From Page.'
+          }
+        });
+      }
+    }
+	
+	if (interaction.data.name === 'httpheaders') {
+      const url = interaction.data.options[0].value;
+      try {
+        const response = await axios.get(`https://api.hackertarget.com/httpheaders/?q=${encodeURIComponent(url)}`);
+        const resulthttpheaders = response.data;
+
+        return res.send({
+          type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+          data: {
+            embeds: [
+              {
+                description: resulthttpheaders,
+                color: null
+              }
+            ]
+          }
+        });
+      } catch (error) {
+        console.log(error);
+        return res.send({
+          type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+          data: {
+            content: 'Failed to HTTP Header Check.'
+          }
+        });
+      }
+    }
 
     if (interaction.data.name === 'qrcode') {
       const text = interaction.data.options.find((option) => option.name === 'text').value;
@@ -359,7 +382,14 @@ app.post('/interactions', verifyKeyMiddleware(PUBLIC_KEY), async (req, res) => {
       return res.send({
         type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
         data: {
-          content: qrCodeUrl,
+		embeds: [
+		{
+			images: {
+				url: qrCodeUrl
+			}
+			color: null
+		}
+		]
         }
       });
     }
@@ -456,11 +486,11 @@ app.get('/register_commands', async (req, res) => {
     },
 	{
       "name": "dnslookup",
-      "description": "Performs a DNS lookup for a given URL and returns the corresponding IP address",
+      "description": "Performs a DNS lookup for a given domain and returns the corresponding IP address",
       "options": [
         {
-          "name": "url",
-          "description": "The URL to perform the DNS lookup on",
+          "name": "domain",
+          "description": "The domain to perform the DNS lookup on",
           "type": 3,
           "required": true
         }
@@ -492,11 +522,11 @@ app.get('/register_commands', async (req, res) => {
     },
 	{
       "name": "hostsearch",
-      "description": "Finds DNS host records (subdomains) for a given URL",
+      "description": "Finds DNS host records (subdomains) for a given domain",
       "options": [
         {
-          "name": "url",
-          "description": "The URL for which to find DNS host records (subdomains)",
+          "name": "domain",
+          "description": "The domain for which to find DNS host records (subdomains)",
           "type": 3,
           "required": true
         }
@@ -509,6 +539,30 @@ app.get('/register_commands', async (req, res) => {
         {
           "name": "ip_or_domain",
           "description": "The IP address or domain for which to perform the reverse IP lookup",
+          "type": 3,
+          "required": true
+        }
+      ]
+    },
+	{
+      "name": "pagelinks",
+      "description": "Extracts links from a web page and returns a list of the extracted links",
+      "options": [
+        {
+          "name": "url",
+          "description": "The URL of the web page from which to extract links",
+          "type": 3,
+          "required": true
+        }
+      ]
+    },
+	{
+      "name": "httpheaders",
+      "description": "Performs an HTTP header check on a given URL and returns the headers of the HTTP response",
+      "options": [
+        {
+          "name": "url",
+          "description": "The URL to perform the HTTP header check on",
           "type": 3,
           "required": true
         }
